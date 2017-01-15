ac36495ed791a1b671e258122d1f0cc219eacfa4
Sebastien Cheraux <scheraux@gmail.com>
2017-01-15 14:50:47 -0800

    adjust Heroku deploy

--- config/puma.rb
+++ config/puma.rb
@@ -12,4 +12,8 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
+
+  if defined?(Resque)
+    Resque.redis = ENV["REDISCLOUD_URL"] || 'localhost:6379'
+  end
 end
